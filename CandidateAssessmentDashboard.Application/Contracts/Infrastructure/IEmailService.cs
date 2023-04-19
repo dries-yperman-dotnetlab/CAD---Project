b586e749e9cd6682a4f296ef5449aff5ec6dc6dd
Dries Yperman <dries.yperman@dotnetlab.eu>
2023-04-19 15:42:58 +0200

    intitial infrastructure Example for MailService

---
+++ CandidateAssessmentDashboard.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using CandidateAssessmentDashboard.Application.Models.Mail;
+
+namespace CandidateAssessmentDashboard.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
---
+++ CandidateAssessmentDashboard.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using CandidateAssessmentDashboard.Application.Contracts.Infrastructure;
+using CandidateAssessmentDashboard.Application.Models.Mail;
+using CandidateAssessmentDashboard.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CandidateAssessmentDashboard.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
